47e4b3bad82a8801dcf25136deeaed1603c91bd2
saintclair <saintclair.com@gmail.com>
2014-05-08 21:29:58 +0000

    nodemailer in progress...

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 var express = require('express');
 var http = require('http');
-//var fs = require('fs');
-//var https = require('https');
-//var privatekey = fs.readFileSync('sslcert/server.key','utf8');
-//var certificate = fs.readFileSync('sslcert/server.crt','utf8');
-
-//var credentials = {key: privateKey, cert: certificate };
 var https = require('https');
 var fs = require('fs');
-//var crypto = require('crypto');
 var options = {
   key: fs.readFileSync(__dirname + '/ssl/key.pem'),
   cert: fs.readFileSync(__dirname + '/ssl/key-cert.pem')
@@ -19,8 +12,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//var nodemailer = require('nodemailer');
+
 var routes = require('./routes');
-var users = require('./routes/user');
+//var contato = require('./routes/contato');
 
 var app = express();
 https.createServer(options, app).listen(443);
@@ -47,7 +42,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(app.router);
 
 app.get('/', routes.index);
-app.get('/users', users.list);
+//app.get('/contato',contato.index);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
